cae6fb717e389a6264a95391e1c315cddea94917
seyonr <seyonr@SR-Macrook.local>
2025-11-10 20:55:22 -0500

    Part 3 Try 1

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 # app.py
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
 
 @app.route('/hello', methods=['GET'])
 def hello():
     return jsonify(message="Hello, World!")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
